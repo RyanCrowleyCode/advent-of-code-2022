27f10ad4526c74bc1a2ad3d8251a9a4c0b0f7ff8
Ryan Crowley <ryan.crowley@daveramsey.com>
2022-12-02 15:36:25 -0600

    outlining files

---
+++ day-eight/puzzle_2.rb
@@ -0,0 +1,10 @@
+require 'pry'
+
+################ Getting the data ##########################
+@file = File.read('data.csv').split("\n")
+
+
+################ Solving the problem ##########################
+
+
+################ Showing the Answer ##########################
